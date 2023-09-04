fd015202cdefd3b66445a34df76a4b70b147bf64
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-04 11:24:25 +0200

    update on uncontrolled

--- contactForm/src/components/UncontrolledForm/uncontrolledForm.jsx
+++ contactForm/src/components/UncontrolledForm/uncontrolledForm.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react'
+
 import style from './uncontrolledForm.module.scss'
 
-export const UncontrolledForm = () => {
+    export const UncontrolledForm = () => { 
+
+        const [error, setError] = useState("")
+
+    function onSubmitForm (event) {
+        event.preventDefault()
+
+        console.log ("first, ",event.target.firstname.value)
+        console.log ("lastname, ",event.target.lastname.value)
+        console.log ("email, ",event.target.email.value)
+
+        if (event.target.firstname.value == ""){
+            setError("Firstname must be set")
+        }
+    }
+
     return(
 
-        <form className={style.formStyle}>
+        <form onSubmit = {(event) => onSubmitForm(event)} className={style.formStyle}>
             <label htmlFor = "firstname">
                 First name: 
                 <input name= "firstname" placeholder="Enter your name here"></input>
             </label>
+            <b>{error}</b>
 
             <label htmlFor = "lastname">
                 Last name: 
@@ -18,6 +36,7 @@ export const UncontrolledForm = () => {
                 Email: 
                 <input id= "email" placeholder="Enter your email here"></input>
             </label>
+            <input type = "submit" value = {"Send"}></input>
         </form>
 
         
